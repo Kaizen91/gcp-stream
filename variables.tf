b82d04439ffa51c733e5c86bdf94346756edb996
Stephen Lang <stephenlang91@gmail.com>
2023-12-26 22:42:45 +0100

    initial terraform set up

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "Project where the dataset and table are created."
+}
+
+variable "delete_contents_on_destroy" {
+  description = "(Optional) If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
+  type        = bool
+  default     = null
+}
+
+variable "force_destroy" {
+  description = "When deleting a bucket, this boolean option will delete all contained objects. If false, Terraform will fail to delete buckets which contain objects."
+  type        = bool
+  default     = false
+}
+
+variable "gcp_region" {
+  description = "GCP region to deploy resources in."
+  type = string
+  default = "us-west2"
+}
